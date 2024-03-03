9b2f8ba0081c478e438279d650735d1dd63e85e9
David Hormigo Ramírez <dhorram948@g.educaand.es>
2024-03-03 18:52:31 +0100

    Creación de proyecto

---
+++ src/main/java/com/turing/alan/cpifp/incidentify/IncidentifyApplication.java
@@ -0,0 +1,13 @@
+package com.turing.alan.cpifp.incidentify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turing/alan/cpifp/incidentify/IncidentifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turing.alan.cpifp.incidentify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
